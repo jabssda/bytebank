3b3d878914e7385be65feb130d800af951bfcfcf
JABS <jabssda@gmail.com>
2022-11-02 16:27:22 -0300

    criação classe gerente

---
+++ src/Gerente.java
@@ -0,0 +1,23 @@
+public class Gerente extends  Funcionario {
+
+
+    private int senha;
+
+    public Gerente() {
+
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha){
+        return  this.senha == senha;
+    }
+
+    public double getBonificacao(){
+        return  this.getSalario();
+    }
+
+
+}
